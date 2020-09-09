c720a1ae5e4bc5f14716075f1a95ba0ce5b74d1f
Doron Enav <doron.enav@yellowbrick.com>
2020-09-08 22:38:01 -0400

    deleted several files which are actually being renamed with new files to improve clarity of use

--- test/test_constants.py
+++
@@ -1,6 +0,0 @@
-"""These constants are loaded by test_run.py"""
-
-self.test_user_name = "test_user"
-self.test_user_password = "Yellowbrick!6"
-self.test_db1 = "util_test_1_db"
-self.test_db2 = "util_test_2_db"
