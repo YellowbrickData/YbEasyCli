38d376f6ed0f3329c1e65083ce1605c05e306d90
Doron Enav <doron.enav@yellowbrick.com>
2020-09-08 23:08:49 -0400

    improved settings file(replaces constant file) used to set up custom users an dbs to run the test cases in personally created db objects

---
+++ test/settings.py
@@ -0,0 +1,8 @@
+"""These settings are loaded by test_run.py"""
+
+host = "yb89"                        #YB server to create test objects in
+create_user_name = "yellowbrick"     #existing user with create user and create db privilege
+test_user_name = "dze"               #user name created to own test objects
+test_user_password = "Yellowbrick!6" #test user password
+test_db1 = "dze_db1"				  #first db created for test objects 
+test_db2 = "dze_db2"				  #second db created for test objects
