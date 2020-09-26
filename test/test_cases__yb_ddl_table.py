b09ff4adbf72af47c850a814a32edf778a0912e1
Doron Enav <doron.enav@yellowbrick.com>
2020-09-25 20:09:32 -0400

    added test case for with_rowcount option

--- test/test_cases__yb_ddl_table.py
+++ test/test_cases__yb_ddl_table.py
@@ -55,4 +55,35 @@ CREATE TABLE {db1}."Prod".a1_t (
 )
 DISTRIBUTE ON (col1);"""
         , stderr='')
+
+    , test_case(
+        cmd=
+            ('yb_ddl_table.py @{argsdir}/db1 --schema_in dev '
+            """--with_db  --with_rowcount --table_in data_types_t --""")
+        , exit_code=0
+        , stdout="""--Rowcount: 1,000,000  Table: {db1}.dev.data_types_t  At: 2020-09-25 16:47:47.103207-07
+CREATE TABLE dze_db1.dev.data_types_t (
+    col1 BIGINT,
+    col2 INTEGER,
+    col3 SMALLINT,
+    col4 NUMERIC(18,0),
+    col5 REAL,
+    col6 DOUBLE PRECISION,
+    col7 UUID,
+    col8 CHARACTER VARYING(256),
+    col9 CHARACTER(1),
+    col10 DATE,
+    col11 TIME WITHOUT TIME ZONE,
+    col12 TIMESTAMP WITHOUT TIME ZONE,
+    col13 TIMESTAMP WITH TIME ZONE,
+    col14 IPV4,
+    col15 IPV6,
+    col16 MACADDR,
+    col17 MACADDR8,
+    col18 BOOLEAN,
+    col19 INTEGER
+)
+DISTRIBUTE ON (col1);"""
+        , stderr=''
+        , map_out={r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}[^\s]*' : 'YYYY-MM-DD HH:MM:SS'})
 ]
