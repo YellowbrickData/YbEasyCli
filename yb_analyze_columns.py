63422ed9ebb745a6a01c07c7be7532b742f9dc06
Doron Enav <doron.enav@yellowbrick.com>
2020-12-02 09:06:01 -0500

    added new feature --with_grouping

--- yb_analyze_columns.py
+++ yb_analyze_columns.py
@@ -24,23 +24,33 @@ class analyze_columns(util):
     def execute(self):
         filter_clause = self.db_filter_args.build_sql_filter(self.config['db_filter_args'])
 
-        sys.stdout.write('-- Running column analysis.\n')
-
-        cmd_results = self.db_conn.call_stored_proc_as_anonymous_block(
+        self.cmd_results = self.db_conn.call_stored_proc_as_anonymous_block(
                 'yb_analyze_columns_p'
                 , args = {
                     'a_dbname' : self.db_conn.database
                     , 'a_tablename' : self.args_handler.args.table
-                    , 'a_filter_clause' : filter_clause})
+                    , 'a_filter_clause' : filter_clause
+                    , 'a_with_grouping' : self.args_handler.args.with_grouping})
 
-        cmd_results.write(tail='-- Completed column analysis.\n')
 
-        exit(cmd_results.exit_code)
-
+    def additional_args(self):
+        args_chunk_o_grp = self.args_handler.args_parser.add_argument_group(
+            'optional analyze argument')
+        args_chunk_o_grp.add_argument("--with_grouping", action="store_true"
+            , help="perform additional grouping analysis, defaults to FALSE,"
+                " this analysis may take a siginificant amount of time to complete"
+                " on large tables.")
 
 def main():
     acs = analyze_columns()
+
+    sys.stdout.write('-- Running column analysis.\n')
+
     acs.execute()
+
+    acs.cmd_results.write(tail='-- Completed column analysis.\n')
+
+    exit(acs.cmd_results.exit_code)
 
 if __name__ == "__main__":
     main()
