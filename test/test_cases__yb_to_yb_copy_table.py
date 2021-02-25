7fb02ed3954b5037d98b758fac7321c429d00d84
Doron Enav <doron.enav@yellowbrick.com>
2021-02-25 13:05:52 -0500

    added parallel threading for performance

--- test/test_cases__yb_to_yb_copy_table.py
+++ test/test_cases__yb_to_yb_copy_table.py
@@ -6,7 +6,8 @@ test_cases = [
         cmd="""yb_to_yb_copy_table.py @{argsdir}/src_db1_dst_db2 --unload_where_clause "col1 <= 2560" """
             """ --src_table dev.data_types_t --dst_table Prod.data_types_t --log_dir tmp"""
         , exit_code=0
-        , stdout="""2020-10-11 16:32:54.529 [ INFO] <main>  SUCCESSFUL BULK LOAD: Loaded 2560 good rows in   0:00:06 (READ: 109.6KB/s WRITE: 58.92KB/s)"""
+        , stdout="""---------------- chunk1of1 -------------------
+2020-10-11 16:32:54.529 [ INFO] <main>  SUCCESSFUL BULK LOAD: Loaded 2560 good rows in   0:00:06 (READ: 109.6KB/s WRITE: 58.92KB/s)"""
         , stderr=''
         , map_out=map_out)
 
@@ -24,8 +25,11 @@ test_cases = [
                 """ --src_table dev.data_types_t --dst_table Prod.data_types_t --chunk_rows 1000"""
                 """ --delimiter '0x01' --log_dir tmp --log_prefix data_type_t_"""
             , exit_code=0
-            , stdout="""2020-10-11 16:50:57.095 [ INFO] <main>  SUCCESSFUL BULK LOAD: Loaded 1000 good rows in   0:00:06 (READ: 43.59KB/s WRITE: 23.43KB/s)
+            , stdout="""---------------- chunk1of3 -------------------
+2020-10-11 16:50:57.095 [ INFO] <main>  SUCCESSFUL BULK LOAD: Loaded 1000 good rows in   0:00:06 (READ: 43.59KB/s WRITE: 23.43KB/s)
+---------------- chunk2of3 -------------------
 2020-10-11 16:51:04.219 [ INFO] <main>  SUCCESSFUL BULK LOAD: Loaded 1000 good rows in   0:00:06 (READ: 43.88KB/s WRITE: 23.61KB/s)
+---------------- chunk3of3 -------------------
 2020-10-11 16:51:11.391 [ INFO] <main>  SUCCESSFUL BULK LOAD: Loaded 560 good rows in   0:00:06 (READ: 24.65KB/s WRITE: 13.22KB/s)"""
             , stderr=''
             , map_out=map_out)
--- yb_to_yb_copy_table.py
+++ yb_to_yb_copy_table.py
@@ -20,7 +20,7 @@ import re
 import random
 from datetime import datetime
 
-from yb_common import ArgsHandler, Common, DBConnect, IntRange, Text, Util
+from yb_common import ArgsHandler, Cmd, Common, DBConnect, IntRange, Text, Util
 from yb_chunk_dml_by_integer import chunk_dml_by_integer
 
 class yb_to_yb_copy_table(Util):
@@ -93,9 +93,13 @@ class yb_to_yb_copy_table(Util):
             "--unload_where_clause", dest='where_clause'
             , help=("SQL clause which filters the rows to copy from the source table"))
         copy_table_o_grp.add_argument(
-            "--chunk_rows", dest="chunk_rows"
+            "--chunk_rows", dest="chunk_rows", metavar='ROWS'
             , type=IntRange(1,9223372036854775807)
             , help="when set data copying will be performed in chunks of rows rather than one big copy")
+        copy_table_o_grp.add_argument(
+            "--threads"
+            , type=IntRange(1,20), default=1
+            , help="when set data copying will be performed in parallel ybunload/ybload threads")
 
     def set_db_connections(self):
         pwd = os.environ['YBPASSWORD'] if 'YBPASSWORD' in os.environ else None
@@ -117,8 +121,23 @@ class yb_to_yb_copy_table(Util):
         elif dst_pwd:
             del os.environ['YBPASSWORD']
 
-    def build_log_file_name_template(self):    
-        self.log_file_name_template = ('{}{}{}_{}_{{{{XofX}}}}_{{log_type}}.log'.format(
+    def additional_args_process(self):
+        #thread use may have severe impact on the YB cluster, so I'm limiting it to super users
+        if (self.args_handler.args.threads > 1
+            and not self.src_conn.ybdb['is_super_user']
+            and not self.dst_conn.ybdb['is_super_user']):
+            Common.error(Text.color(
+                "The '--threads' option is only supported for YBDB super users."
+                , 'yellow'))
+
+        if (self.args_handler.args.chunk_rows
+            and self.src_conn.ybdb['version_major'] < 4):
+            Common.error(Text.color(
+                "The '--chunk_rows' option is only supported on YBDB version 4 or higher."
+                " The source db is running YBDB %s..." % self.src_conn.ybdb['version']
+                , 'yellow'))
+            
+        self.log_file_name_template = ('{}{}{}_{}_{{{{CofC}}}}{{{{TofT}}}}_{{log_type}}.log'.format(
             ('%s/' % self.args_handler.args.log_dir
                 if self.args_handler.args.log_dir
                 else '')
@@ -190,12 +209,6 @@ class yb_to_yb_copy_table(Util):
             , ybload_cmd = ybload_cmd)
 
     def chunk_table_unload_sql(self, table_unload_sql):
-        if self.src_conn.ybdb['version_major'] < 4:
-            Common.error(Text.color(
-                "The '--chunk_rows' option is only supported on YBDB version 4 or higher."
-                " The source db is running YBDB %s..." % self.src_conn.ybdb['version']
-                , 'yellow'))
-
         self.args_handler.args.dml = ("%s AND <chunk_where_clause>" % table_unload_sql)
         self.args_handler.args.execute_chunk_dml = False
         self.args_handler.args.verbose_chunk_off = False
@@ -220,44 +233,69 @@ class yb_to_yb_copy_table(Util):
             , where_clause=(' AND %s' % self.args_handler.args.where_clause if self.args_handler.args.where_clause else ''))
 
         if self.args_handler.args.chunk_rows:
-            table_unload_sql = self.chunk_table_unload_sql(table_unload_sql)
+            chunks_sql = self.chunk_table_unload_sql(table_unload_sql)
         else:
-            table_unload_sql = [table_unload_sql]
+            chunks_sql = [table_unload_sql]
 
         os.environ['SRC_YBPASSWORD'] = self.src_conn.env['pwd']
         os.environ['DST_YBPASSWORD'] = self.dst_conn.env['pwd']
 
-        seq = 1
-        format_XofX = '%.0{len}dof%.0{len}d'.format(len=len(str(len(table_unload_sql))))
-        for unload_sql in table_unload_sql:
-            XofX = format_XofX % (seq, len(table_unload_sql))
+        total_chunks = len(chunks_sql)
+        format_CofC = 'chunk%.0{len}dof%.0{len}d'.format(len=len(str(total_chunks)))
+        total_threads = self.args_handler.args.threads
+        format_TofT = '_thread%.0{len}dof%.0{len}d'.format(len=len(str(total_threads)))
+        TofT = ''
+        thread_clause = ''
+        for chunk in range(1,total_chunks+1):
+            unload_sql = chunks_sql[chunk-1]
+            CofC = format_CofC % (chunk, total_chunks)
 
-            copy_cmd = self.table_copy_cmd.format(
-                unload_sql=unload_sql, XofX=XofX)
+            cmd_threads = []
+            for thread in range(1,total_threads+1):
+                if total_threads > 1:
+                    thread_clause = ' AND /* thread_clause(thread: %d) >>>*/ rowunique %% %d = %d /*<<< thread_clause */' % (thread, total_threads, thread-1)
+                    TofT = format_TofT % (thread, total_threads)
+                copy_cmd = self.table_copy_cmd.format(
+                    unload_sql=unload_sql.rstrip().rstrip(';') + thread_clause
+                    , CofC=CofC
+                    , TofT=TofT)
+                cmd = Cmd(copy_cmd, False, wait=False)
+                cmd.TofT = TofT
+                cmd_threads.append(cmd)
 
-            ybload_log_file_name = self.log_file_name_template.format(
-                log_type='ybload').format(XofX=XofX)
+            thread_exit_code = 0
+            thread_failed = False
+            for cmd in cmd_threads:
+                cmd.wait()
 
-            cmd_results = Common.call_cmd(copy_cmd, False)
+                loaded = False
+                print('---------------- %s%s -------------------'
+                    % (CofC, cmd.TofT))
+                if cmd.exit_code == 0:
+                    ybload_log_file_name = self.log_file_name_template.format(
+                        log_type='ybload').format(
+                            CofC=CofC
+                            , TofT=cmd.TofT)
+                    file = open(ybload_log_file_name, "r")
+                    for line in file:
+                        if re.search('SUCCESSFUL BULK LOAD', line):
+                            loaded = True
+                            sys.stdout.write(line)
+                            break
 
-            loaded = False
-            if cmd_results.exit_code == 0:
-                file = open(ybload_log_file_name, "r")
-                for line in file:
-                    if re.search('SUCCESSFUL BULK LOAD', line):
-                        loaded = True
-                        sys.stdout.write(line)
-                        break
+                if not loaded:
+                    cmd.write()
+                    log_file_name = self.log_file_name_template.format(
+                        log_type='*').format(
+                            CofC=CofC
+                            , TofT=cmd.TofT)
+                    print('Table Copy {}, please review the log files: {}'.format(
+                        Text.color('Failed', 'red'), log_file_name))
+                    thread_exit_code = cmd.exit_code
+                    thread_failed = True
 
-            if not loaded:
-                cmd_results.write()
-                log_file_name = self.log_file_name_template.format(
-                    log_type='*').format(XofX=XofX)
-                print('Table Copy {}, please review the log files: {}'.format(
-                    Text.color('Failed', 'red'), log_file_name))
-                exit(cmd_results.exit_code)
-
-            seq += 1
+            if thread_failed:
+                exit(thread_exit_code)
 
         del os.environ['SRC_YBPASSWORD']
         del os.environ['DST_YBPASSWORD']
@@ -270,7 +308,8 @@ def main():
 
     ytoy.set_db_connections()
 
-    ytoy.build_log_file_name_template()
+    ytoy.additional_args_process()
+
     ytoy.build_table_copy_cmd()
 
     ytoy.execute()
