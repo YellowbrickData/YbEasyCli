c6c411c1d4b8e15c63ce3e59ee266738caabb8bf
rkickhofel <65052913+rkickhofel@users.noreply.github.com>
2022-12-28 17:25:12 -0800

    Added  : catalog_storage_by_db_p, catalog_storage_by_table_p, column_p, column_values_p, log_replica_p, stmt_topn_p, storage_by_worker_p.sql, table_p, version_p, view_validate_p Updated: sysviews_create.sql accordingly.

---
+++ sql/sysviews_yb5/catalog_storage_by_table_p.sql
@@ -0,0 +1,195 @@
+/* ****************************************************************************
+** catalog_storage_by_table_p.sql
+**
+** Storage space of catalog tables.
+**
+** Usage:
+**   See COMMENT ON FUNCTION statement after CREATE PROCEDURE.
+**
+** (c) 2022 Yellowbrick Data Corporation.
+** . This script is provided free of charge by Yellowbrick Data Corporation as a 
+**   convenience to its customers.
+** . This script is provided "AS-IS" with no warranty whatsoever.
+** . The customer accepts all risk in connection with the use of this script, and
+**   Yellowbrick Data Corporation shall have no liability whatsoever.
+**
+** Revision History:
+** . 2022.12.27 - YbEasyCli inclusion.
+** . 2022.07.07 - Yellowbrick Technical Support  
+*/
+
+/* ****************************************************************************
+**  Example results:
+**
+**    db_name   | table_id | schema_name |         table_name         | row_est | total_size | table_size | index_size | toast_size
+** -------------+----------+-------------+----------------------------+---------+------------+------------+------------+------------
+**  yellowbrick |     9901 | sys         | yb_query_execution_analyze |  444734 | 401 MB     | 391 MB     | 10016 kB   | 32 kB
+**  yellowbrick |     9927 | sys         | yb_query_plan              | 1810851 | 377 MB     | 234 MB     | 142 MB     |
+**  yellowbrick |     5003 | sys         | shardstore                 |  313328 | 176 MB     | 87 MB      | 89 MB      |
+**  yellowbrick |     4259 | pg_catalog  | yb_deletes_pg_shdepend     | 1170968 | 144 MB     | 116 MB     | 28 MB      |
+*/
+
+/* ****************************************************************************
+** Create a table to define the rowtype that will be returned by the procedure.
+*/
+DROP TABLE IF EXISTS catalog_storage_by_table_t CASCADE
+;
+
+CREATE TABLE catalog_storage_by_table_t
+(
+   db_name        VARCHAR(128)     
+ , table_id       BIGINT      
+ , schema_name    VARCHAR(128)              
+ , table_name     VARCHAR(128)   
+ , row_est        BIGINT 
+ , total_size     VARCHAR(32)                             
+ , table_size     VARCHAR(32)                               
+ , index_size     VARCHAR(32)                                
+ , toast_size     VARCHAR(32)                               
+)
+;
+
+
+/* ****************************************************************************
+** Create the procedure.
+*/
+CREATE PROCEDURE catalog_storage_by_table_p( 
+      _db_ilike       VARCHAR DEFAULT '%'
+    , _schema_ilike   VARCHAR DEFAULT '%'
+    , _table_ilike    VARCHAR DEFAULT '%'
+    , _yb_util_filter VARCHAR DEFAULT 'TRUE' 
+   )
+   RETURNS SETOF catalog_storage_by_table_t 
+   LANGUAGE 'plpgsql' 
+   VOLATILE
+   SECURITY DEFINER
+AS 
+$proc$
+DECLARE
+
+   _debug     INTEGER      := 0;
+   _db_id     BIGINT;
+   _db_name   VARCHAR( 128 );   
+   _db_rec    RECORD;  
+   _dbs_sql   TEXT         := '';
+   _rec       RECORD;
+   _ret_rec   catalog_storage_by_table_t%ROWTYPE;   
+   _sql       TEXT         := '';
+   
+   _fn_name   VARCHAR(256) := 'catalog_storage_by_table_p';
+   _prev_tags VARCHAR(256) := current_setting('ybd_query_tags');
+   _tags      VARCHAR(256) := CASE WHEN _prev_tags = '' THEN '' ELSE _prev_tags || ':' END || 'sysviews:' || _fn_name;    
+  
+BEGIN  
+
+   EXECUTE 'SET ybd_query_tags  TO ' || quote_literal( _tags );
+   PERFORM sql_inject_check_p('_yb_util_filter', _yb_util_filter);   
+   
+   -- Get the list of databases to iterate over.
+   _dbs_sql = 'SELECT 
+      database_id AS db_id
+    , name        AS db_name 
+   FROM sys.database 
+   WHERE name ILIKE ' || quote_literal( _db_ilike ) || ' 
+   ORDER BY name
+   ';      
+   
+   IF ( _debug > 0 ) THEN RAISE INFO '_dbs_sql=%', _dbs_sql; END IF;
+     
+
+   -- Iterate over each db and get the table sizes and metadata. 
+   FOR _db_rec IN EXECUTE _dbs_sql 
+   LOOP
+   
+      _db_id   := _db_rec.db_id ;
+      _db_name := _db_rec.db_name ;
+      IF ( _debug > 0 ) THEN RAISE INFO '_db_id=%, _db_name=%',_db_id, _db_name ; END IF;   
+
+      _sql := 'SELECT
+   ' || quote_literal(_db_name) || '::VARCHAR(128) AS db_name
+      , table_id::BIGINT                           AS table_id
+      , table_schema::VARCHAR(128)                 AS table_schema
+      , table_name::VARCHAR(128)                   AS table_name
+      , row_estimate::BIGINT                       AS row_est 
+      , pg_size_pretty(total_bytes)::VARCHAR(32)   AS total_size
+      , pg_size_pretty(table_bytes)::VARCHAR(32)   AS table_size
+      , pg_size_pretty(index_bytes)::VARCHAR(32)   AS index_size
+      , pg_size_pretty(toast_bytes)::VARCHAR(32)   AS toast_size
+      FROM
+        ( SELECT *
+          , total_bytes - index_bytes - COALESCE(toast_bytes, 0) AS table_bytes
+          FROM
+            ( SELECT
+           ' || quote_literal(_db_name) || '::VARCHAR(128) AS db_name
+              , c.oid                                      AS table_id
+              , nspname                                    AS table_schema
+              , relname                                    AS table_name
+              , c.reltuples                                AS row_estimate
+              , pg_total_relation_size(c.oid)              AS total_bytes
+              , pg_indexes_size(c.oid)                     AS index_bytes
+              , pg_total_relation_size(reltoastrelid)      AS toast_bytes
+              , CASE WHEN (n.nspname = ''sys'' OR table_name = ''yb_deletes_pg_shdepend'')
+                          AND db_name != ''yellowbrick''
+                     THEN ''f''
+                     ELSE ''t''
+                END::BOOLEAN                               AS to_display
+              FROM
+            ' || quote_ident(_db_name) || '.pg_catalog.pg_class                   c
+                LEFT JOIN ' || quote_ident(_db_name) || '.pg_catalog.pg_namespace n
+                  ON n.oid = c.relnamespace
+              WHERE
+                relkind = ''r''
+                AND total_bytes > 0
+                AND c.relname ILIKE ' || quote_literal(_table_ilike)  || '
+                AND n.nspname ILIKE ' || quote_literal(_schema_ilike) || '
+                AND to_display = ''t''
+                AND nspname NOT IN ( ''information_schema'')                
+            ) pc
+        )     a
+      ORDER BY
+        total_bytes DESC 
+      ';
+
+      RETURN QUERY EXECUTE _sql; 
+   
+   END LOOP;   
+
+   -- Reset ybd_query_tags back to its previous value
+   EXECUTE 'SET ybd_query_tags  TO ' || quote_literal( _prev_tags ); 
+   
+END;   
+$proc$ 
+;
+
+
+COMMENT ON FUNCTION catalog_storage_by_table_p(VARCHAR, VARCHAR, VARCHAR, VARCHAR) IS 
+$cmnt$Description:
+Size of catalog tables across all databases.
+  
+Examples:
+  SELECT * FROM catalog_storage_by_table_p() WHERE total_size ~ '(GB|\d{3} MB)';
+  SELECT * FROM catalog_storage_by_table_p( 'yellowbrick', 'sys');
+  SELECT * FROM catalog_storage_by_table_p( 'yellowbrick', 'sys', '%log%' );
+  
+Arguments:
+. _db_ilike       - (optional) An ILIKE pattern for the database name. i.e. '%dim%'.
+. _schema_ilike   - (optional) An ILIKE pattern for the schema name. i.e. 'sys'.
+. _table_ilike    - (optional) An ILIKE pattern for the table name.  i.e. 'log%'.
+. _yb_util_filter - (internal) Used by YbEasyCli.
+
+Notes:
+. The sys schema and pg_catalog.yb_deletes_pg_shdepend table are global and so
+  are reported only under the yellowbrick database.
+. Sizes are pretty printed so are text, not numeric.  
+
+Example Result:
+   db_name   | table_id | schema_name |  table_name   | row_est | total_size | table_size | index_size | toast_size
+-------------+----------+-------------+---------------+---------+------------+------------+------------+------------
+ yellowbrick |     9927 | sys         | yb_query_plan | 4090043 | 832 MB     | 467 MB     | 364 MB     |
+ yellowbrick |     5003 | sys         | shardstore    |  195884 | 313 MB     | 155 MB     | 158 MB     |
+ 
+Version:
+. 2022.12.27 - Yellowbrick Technical Support
+$cmnt$
+;
+
---
+++ sql/sysviews_yb5/log_replica_p.sql
@@ -0,0 +1,148 @@
+/* ****************************************************************************
+** log_replica_p.sql
+**
+** Currently running and historical replication statements.
+**
+** Usage:
+**   See COMMENT ON FUNCTION statement after CREATE PROCEDURE.
+**
+** (c) 2022 Yellowbrick Data Corporation.
+** . This script is provided free of charge by Yellowbrick Data Corporation as a 
+**   convenience to its customers.
+** . This script is provided "AS-IS" with no warranty whatsoever.
+** . The customer accepts all risk in connection with the use of this script, and
+**   Yellowbrick Data Corporation shall have no liability whatsoever.
+**
+** Revision History:
+** . 2022.12.27 - Added _yb_util_filter
+**                YbEasyCli inclusion
+** . 2022.08.29 - Yellowbrick Technical Support
+*/
+
+/* ****************************************************************************
+**  Example results:
+**
+**  db_name | replica_name  |             snapshot_id              | state | error |     start_time      |      end_time       |   duration   | mb_sent | mb/sec | backup_session_key
+** ---------+---------------+--------------------------------------+-------+-------+---------------------+---------------------+--------------+---------+--------+--------------------
+**  denav   | denav_replica | odsReplication2021_22_08_22_19_36_38 | DONE  |       | 2022-08-22 19:36:38 | 2022-08-22 19:37:52 | 00:01:14.439 |     0.0 |      0 | ...XDfrcQY=
+**  kchou   | kchou_replica | kchou_replica_22_08_22_19_36_37      | DONE  |       | 2022-08-22 19:36:37 | 2022-08-22 19:37:52 | 00:01:15.616 |     0.0 |      0 | ...bzfFgsur
+**  denav   | denav_replica | odsReplication2021_22_08_22_19_35_38 | DONE  |       | 2022-08-22 19:35:38 | 2022-08-22 19:36:32 | 00:00:53.82  |     0.0 |      0 | ...E2gy2GfP
+*/
+
+/* ****************************************************************************
+** Create a table to define the rowtype that will be returned by the procedure.
+*/
+DROP TABLE IF EXISTS log_replica_t CASCADE
+;
+
+CREATE TABLE log_replica_t
+(
+   db_name            VARCHAR (128)
+ , replica_name       VARCHAR (256)
+ , snapshot_id        VARCHAR (256)
+ , state              VARCHAR (256)
+ , error              VARCHAR (256)
+ , start_time         TIMESTAMP
+ , end_time           TIMESTAMP
+ , duration           TIME
+ , mb_sent            NUMERIC (19, 1)
+ , "mb/sec"           INTEGER
+ , backup_session_key VARCHAR (256)
+)
+;
+
+
+/* ****************************************************************************
+** Create the procedure.
+*/
+CREATE PROCEDURE log_replica_p(
+        _from_ts        TIMESTAMP DEFAULT  (DATE_TRUNC('week', CURRENT_DATE)::DATE - 7) 
+      , _to_ts          TIMESTAMP DEFAULT  CURRENT_TIMESTAMP
+      , _yb_util_filter VARCHAR   DEFAULT 'TRUE'       
+   )
+   RETURNS SETOF log_replica_t
+   LANGUAGE 'plpgsql' 
+   VOLATILE
+   CALLED ON NULL INPUT
+   SECURITY DEFINER
+AS 
+$proc$
+DECLARE
+
+   _sql       TEXT         := '';
+   
+   _fn_name   VARCHAR(256) := 'log_replica_p';
+   _prev_tags VARCHAR(256) := current_setting('ybd_query_tags');
+   _tags      VARCHAR(256) := CASE WHEN _prev_tags = '' THEN '' ELSE _prev_tags || ':' END || 'sysviews:' || _fn_name;    
+  
+BEGIN  
+
+   -- Append sysviews:log_replica to ybd_query_tags
+   EXECUTE 'SET ybd_query_tags  TO '|| quote_literal( _tags );
+   
+   -- To prevent SQL injection attack.
+   PERFORM sql_inject_check_p('_yb_util_filter', _yb_util_filter);  
+
+   _sql := $$WITH replica_status_union AS
+   (  SELECT *
+       , TRUE AS active
+      FROM sys.replica_status
+      UNION ALL
+      SELECT *
+       , FALSE AS active
+      FROM sys.log_replica_status
+   )
+   SELECT d.name::VARCHAR(128)                                                       AS db_name
+    , r.name::VARCHAR(256)                                                           AS replica_name
+    , snapshot_id::VARCHAR(256)                                                      AS snapshot_id
+    , state::VARCHAR(256)                                                            AS state
+    , nvl2( error_string, SUBSTR( error_string, 1, 64 ), NULL )::VARCHAR(256)        AS error
+    , date_trunc( 'sec', start_time )::TIMESTAMP                                     AS start_time
+    , date_trunc( 'sec', end_time )::TIMESTAMP                                       AS end_time
+    , (NVL( end_time, CURRENT_TIMESTAMP ) - start_time)::TIME                        AS duration
+    , ROUND( sent_bytes / 1024.0^2, 1 )::NUMERIC (19, 1)                             AS mb_sent
+    , ROUND( sent_bytes / extract( epoch FROM( duration ) ), 0 )::INTEGER            AS "mb/sec"
+    , RIGHT( '...' || RIGHT( backup_session_key, 8 ), 64 )::VARCHAR (256)            AS backup_session_key
+   FROM sys.replica             AS r
+      JOIN sys.database         AS d
+   USING( database_id )
+      JOIN replica_status_union AS rs
+   USING( replica_id )
+   WHERE  start_time    >= $$ || quote_literal( _from_ts ) || $$::TIMESTAMP
+      AND start_time    <= $$ || quote_literal( _to_ts   ) || $$::TIMESTAMP
+      AND $$ || _yb_util_filter || $$
+   ORDER BY active DESC, start_time DESC, db_name
+   $$;
+
+   -- RAISE INFO '_sql: %', _sql
+   RETURN QUERY EXECUTE _sql;
+
+   -- Reset ybd_query_tags back to its previous value
+   EXECUTE 'SET ybd_query_tags  TO '|| quote_literal( _prev_tags );
+   
+END;   
+$proc$
+;
+
+   
+COMMENT ON FUNCTION log_replica_p( TIMESTAMP, TIMESTAMP, VARCHAR ) IS 
+$cmnt$Description:
+Currently running and historical replication statements.
+
+Similar to sys.replica_status and sys.log_replica_status.
+  
+Examples:
+  SELECT * FROM log_replica_p(); 
+  SELECT * FROM log_replica_p( '2022-08-08', '2022-08-12 16:00:00' ); 
+  
+Arguments:
+. _from_ts TIMESTAMP (optl)  - Starting TIMESTAMP of replication generated statements. 
+                               Default: begining of previous week (Sunday).
+. _to_ts   TIMESTAMP (optl)  - Ending TIMESTAMP of statements to analyze. 
+                               Default: now().  
+. _yb_util_filter (internal) - For YbEasyCli.
+
+Version:
+. 2022.08.29 - Yellowbrick Technical Support
+$cmnt$
+;
