84f57d92efa5b5c0c506bd91d9b7676f682bdea3
Doron Enav <doron.enav@yellowbrick.com>
2023-02-04 20:30:30 -0500

    fixed query bug related timestamp data type with parenthesis

--- yb_common.py
+++ yb_common.py
@@ -39,7 +39,7 @@ class Common:
     Grouping of attributes in methods commonly use in ybutils
     """
 
-    version = '20221120'
+    version = '20230204'
     verbose = 0
 
     util_dir_path = os.path.dirname(os.path.realpath(sys.argv[0]))
