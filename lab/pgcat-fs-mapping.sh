ffa0314d0dfe9f1d51e4dff1c1792921deb51d3f
Eugene Mindrov <eugene.mindrov@yellowbrick.com>
2023-03-03 16:39:09 -0500

    Minor changes in Lab scripts

--- lab/pgcat-fs-mapping.sh
+++ lab/pgcat-fs-mapping.sh
@@ -7,18 +7,19 @@ export YBDATABASE=yellowbrick
 PGDATA=/mnt/ybdata/ybd/postgresql/build/db/data
 DBSIZE=500
 FNSIZE=100
-while getopts d:s:f: OPT ; do
+while getopts d:s:f:h OPT ; do
 	case $OPT in
 		d) export YBDATABASE=$OPTARG ;;
 		s) DBSIZE=$OPTARG ;;
 		f) FNSIZE=$OPTARG ;;
+		h) echo "Usage: $(basename $0) [-d database, default: $YBDATABASE] [-s database_size_mb_filter, default: $DBSIZE] [-f file_size_mb_filter, default: $FNSIZE]" ; exit ;;
 	esac
 done
 [ $YBDATABASE = 'yellowbrick' ] && CATALOG=global || CATALOG=base/$(ybsql -XAqt -d yellowbrick -c "SELECT database_id FROM sys.database WHERE name = '$YBDATABASE'")
 [ $CATALOG = 'base/' ] && { echo ERROR: could not find \'$YBDATABASE\' database ID; exit 1 ; }
 sudo test -d $PGDATA/$CATALOG || { echo ERROR: could not find $PGDATA/$CATALOG directory ; exit 1 ; }
 
-echo -e "PG catalog mapper v0.2b\nPGDATA: $PGDATA"
+echo -e "PG catalog mapper v0.3b\nPGDATA: $PGDATA"
 
 TEMPDIR=/tmp/fsinfo-$(uuidgen)
 mkdir $TEMPDIR
@@ -56,33 +57,37 @@ GROUP BY 1;
 .head on
 .mode column
 
-.print \033[32m== Database catalog sizes > $DBSIZE MB\033[0m
+.print \n\033[32m== Database catalog sizes > $DBSIZE MB\033[0m
 SELECT b.id, CASE a.dir WHEN 'base' THEN '<total size of ''base'' directory>' WHEN 'global' THEN '<global catalog>' ELSE b.name END AS "database                           |"
-	, a.sizeb/1024/1024 AS cat_size_mb, a.dir AS "path relative to PGDATA"
+	, a.sizeb/1024/1024 AS "^ cat_size_mb", a.dir AS "path relative to PGDATA"
 FROM dbfs a
 	LEFT JOIN dbinfo b ON b.id = substr(a.dir, instr(a.dir,'/')+1)
 WHERE a.sizeb > $DBSIZE*1024*1024
 ORDER BY a.sizeb DESC;
 
-.print \033[32m== File nodes (size > $FNSIZE MB) mapping to PG catalog objects\033[0m
+.print \nDBNAME: $YBDATABASE
+
+.print \n\033[32m== File nodes (size > $FNSIZE MB) mapping to PG catalog objects\033[0m
 SELECT p.fpath AS "file node in PGDATA            |", f.chunks AS fn_chunks
-	, f.sizeb/1024/1024 AS fn_agg_size_mb, p.osize/1024/1024 AS osize_mb, p.isize/1024/1024 AS isize_mb
+	, f.sizeb/1024/1024 AS "^ fn_agg_size_mb", p.osize/1024/1024 AS osize_mb, p.isize/1024/1024 AS isize_mb
         , p.oid, CASE p.relkind WHEN 'r' THEN 'table' WHEN 'i' THEN 'index' WHEN 't' THEN 'toast' ELSE p.relkind END AS otype
 	, p.relfqname AS "fully qualified relation name                   |", p.rowcount, p.created
 FROM fnagg f
         JOIN pgcat p ON p.fpath = '$CATALOG/'||f.filenode
-WHERE fn_agg_size_mb > $FNSIZE
-ORDER BY sizeb DESC;
+WHERE f.sizeb > $FNSIZE*1024*1024
+ORDER BY f.sizeb DESC;
 
 CREATE TEMP TABLE nomap AS
 SELECT filenode FROM fnagg
 WHERE sizeb > $FNSIZE*1024*1024 AND '$CATALOG/'||filenode NOT IN (SELECT fpath FROM pgcat);
 
-.print \033[32m== File nodes (size > $FNSIZE MB) without mapping\033[0m
-SELECT f.filenode AS "file node in PGDATA            |", f.sizeb/1024/1024 AS size_mb
+.print \n\033[32m== File nodes (size > $FNSIZE MB) without mapping\033[0m
+SELECT f.filenode AS "file node in PGDATA/$CATALOG", f.sizeb/1024/1024 AS size_mb
 FROM fsinfo f
 	JOIN nomap n ON f.filenode = n.filenode OR f.filenode LIKE n.filenode||'.%' OR f.filenode LIKE n.filenode||'|_%' ESCAPE '|'
 ORDER BY f.filenode;
 SQL
 cd
+echo -e "\n\033[32m== Stray files (size > $FNSIZE MB), not belonging to any database in particular\033[0m"
+sudo sh -c "cd $PGDATA ; sudo find . ./base -maxdepth 1 -type f -size +${FNSIZE}M -exec ls -lh {} +"
 rm -rf $TEMPDIR
