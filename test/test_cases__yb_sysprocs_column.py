707a7da4f7f689b18ef0bac5df8c0851caa642ff
yb-denav <doron.enav@yellowbrick.com>
2023-06-22 17:22:48 -0400

    fixed test case to map out changing table ids

--- test/test_cases__yb_sysprocs_column.py
+++ test/test_cases__yb_sysprocs_column.py
@@ -1,5 +1,9 @@
 #TODO sysprocs need many more test cases
 #   for regular user test cases the sysview procedures need to be installed
+map_out=[ { 'regex' : re.compile(r'\d{5}'), 'sub' : 'XXXXX' }
+    , { 'regex' : re.compile(r'\d{4}'), 'sub' : 'XXXX' }
+    , { 'regex' : re.compile(r'\d{3}'), 'sub' : 'XXX' } ]
+
 test_cases = [
     test_case(
         cmd="""yb_sysprocs_column.py @{argsdir}/db1 --database_in {db1} {db2} --table_like '%C%'"""
@@ -11,5 +15,5 @@ name        id  type    name      name       id  name    type
 {db1}  61470  view    Prod      C1_v        1  Col1    int4    t
 {db2}  61543  table   Prod      C1_t        1  Col1    int4    t
 {db2}  61561  view    Prod      C1_v        1  Col1    int4    t"""
-        , stderr="")
+        , stderr="", map_out=map_out)
 ]
