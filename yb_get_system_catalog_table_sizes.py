f533c620d54d5b872efe5b2a3310048de594f243
Eugene Mindrov <eugene.mindrov@yellowbrick.com>
2023-12-08 13:44:26 -0500

    - `yb_get_system_catalog_table_sizes.py`: added a new switch to skip global system catalog - Bumped up the version number in `yb_common.py`

--- yb_get_system_catalog_table_sizes.py
+++ yb_get_system_catalog_table_sizes.py
@@ -9,7 +9,7 @@ OPTIONS:
       (yb_get_system_catalog_table_sizes.py --help)
 """
 import os, sys
-from yb_common import Util, Report
+from yb_common import Util, Report, Common
 from tempfile import mkstemp
 # This is strictly for backward compatibility with ancient Python 2.x
 from collections import OrderedDict
@@ -17,8 +17,8 @@ from collections import OrderedDict
 class get_system_catalog_table_sizes(Util):
     units = 'BKMGTP'
     columns = OrderedDict([
-        ('database_id'  , {'value': 'd.oid'                  , 'type': 'BIGINT'}),
-        ('database_name', {'value': 'd.datname'              , 'type': 'VARCHAR(128)'}),
+        ('database_id'  , {'value': "CASE WHEN c.relisshared THEN          0 ELSE d.oid     END", 'type': 'BIGINT'}),
+        ('database_name', {'value': "CASE WHEN c.relisshared THEN '<global>' ELSE d.datname END", 'type': 'VARCHAR(128)'}),
         ('schema_id'    , {'value': 's.oid'                  , 'type': 'BIGINT'}),
         ('schema_name'  , {'value': 's.nspname'              , 'type': 'VARCHAR(128)'}),
         ('table_id'     , {'value': 'c.oid'                  , 'type': 'BIGINT'}),
@@ -47,6 +47,9 @@ class get_system_catalog_table_sizes(Util):
         args_optional_filter_grp.add_argument("--total_size_min"
             , type = int, default = 0
             , help = "limit the report by the 'total size' column >= specified amount")
+        args_optional_filter_grp.add_argument("--skip_global"
+            , action = "store_true"
+            , help = "exclude global system catalog shared tables from the report")
 
     def execute(self):
         self.columns['unit']['value'] = "'{unit}'".format(unit=self.args_handler.args.unit)
@@ -64,7 +67,7 @@ class get_system_catalog_table_sizes(Util):
         # Generate a single ybsql script that connects to all specified databases
         # and runs a local SQL against current system catalog.
         sql = ''
-        for db in self.get_dbs():
+        for rownum, db in enumerate(self.get_dbs()):
             sql += """\n\\c {database}\n
 SELECT\n{columns}
 FROM pg_catalog.pg_class AS c
@@ -72,18 +75,22 @@ FROM pg_catalog.pg_class AS c
     JOIN pg_catalog.pg_database  AS d ON d.datname = '{database}'
 WHERE s.nspname IN ('sys', 'pg_catalog')
     AND c.relkind = 'r'
-    AND CASE '{database}' WHEN 'yellowbrick' THEN TRUE ELSE NOT c.relisshared END
-    AND total_size >= {total_size_min}
+    AND CASE WHEN {rn} = 0 AND {show_global} THEN TRUE ELSE NOT c.relisshared END
+    AND total_size >= {min_size}
     AND {filter_clause};\n""".format(
-          database = db
-        , columns  = '\n,'.join(['{col} AS {dtype}'.format(col=v['value'], dtype=k) for k,v in self.columns.items()])
-        , total_size_min = self.args_handler.args.total_size_min
+          database      = db
+        , rn            = rownum
+        , show_global   = 'FALSE' if self.args_handler.args.skip_global else 'TRUE'
+        , columns       = '\n,'.join(['{col} AS {dtype}'.format(col=v['value'], dtype=k) for k,v in self.columns.items()])
+        , min_size      = self.args_handler.args.total_size_min
         , filter_clause = self.db_filter_sql())
 
         self.cmd_result = self.db_conn.ybsql_query(sql)
         self.cmd_result.on_error_exit()
         # Save the output to a temp file for loading it into a temp table (needed for nice reporting)
         tmp_dat_fd, tmp_dat_path = mkstemp(prefix='YbEasyCli_syscat_', suffix='.dat')
+        # cygwin whatever...
+        tmp_dat_path = Common.os_path(tmp_dat_path)
         with os.fdopen(tmp_dat_fd, 'wb') as tmp:
             tmp.write(self.cmd_result.stdout.strip().encode())
         # Create a pre-SQL step for creating a temp table and populating it with data from the main SELECT
