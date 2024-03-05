1ca35ab04f6e50599c1b99f034818ccbf95b18e4
yb-denav <doron.enav@yellowbrick.com>
2024-03-05 14:10:17 -0500

    Update version number

--- yb_common.py
+++ yb_common.py
@@ -44,7 +44,7 @@ class Common:
     Grouping of attributes in methods commonly use in ybutils
     """
 
-    version = '20231208'
+    version = '20240305'
     verbose = 0
 
     util_dir_path = os.path.dirname(os.path.realpath(sys.argv[0]))
