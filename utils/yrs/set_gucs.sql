5316ceca2a4ef34a471584288a6bbdfafad376ba
robert.kickhofel <robert.kickhofel@yellowbrick.com>
2026-03-20 11:28:13 -0600

    added common connect fns, renamed flush_all, minor reformatting of SQL, flushall now sends table flush msgs to stdout and log file.

--- utils/yrs/set_gucs.sql
+++ utils/yrs/set_gucs.sql
@@ -1,7 +1,13 @@
--- YB 7.x introduces JSONB datatype, so we have to set a specific GUC to be able to touch tables with JSONB columns (example: yb_metering.staging.instance_event):
+-- set_gucs.sql
+-- 
+-- YB 7.x introduces JSONB datatype which is used in in some system tables.
+-- However, the option is not on be default for statements executed by users. 
+-- Enable it so that tables with JSONB columns can be flushed. 
+-- Example: yb_metering.staging.instance_event):
+
 DO $block$
 BEGIN
-  if split_part(current_setting('yb_server_version'), '.', 1)::smallint >= 7 then 
-    set enable_full_json to on;
-  end if;
+  IF split_part(current_setting('yb_server_version'), '.', 1)::SMALLINT >= 7 THEN 
+    SET enable_full_json TO ON;
+  END IF;
 END $block$;
